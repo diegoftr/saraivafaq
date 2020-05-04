1ea053ac023a501cf17a715ab31bbf2ac411ed9c
Diego <diegoftr@gmail.com>
2020-05-04 15:36:24 -0300

    Adcionado suporte ao chat humano

--- App.js
+++ App.js
@@ -18,14 +18,23 @@ import { faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 import { faVolumeMute } from '@fortawesome/free-solid-svg-icons'
 import ButtonGroup from 'react-native-button-group';
 
+import * as Constantes from './constantes';
+
+const URL = Constantes.URL_API_WS_AWS
+
 
 type Props = {};
 export default class App extends Component<Props> {
+  
+  ws = new WebSocket(URL)
 
   state = {
     messages: [],
     ligarSom: true,
-    dataUltimaConsulta: null
+    dataUltimaConsulta: null,
+    respostasIncorretas: 0,
+    chatHumano: false,
+    connectionId: null
   }
 
   constructor(props) {
@@ -33,7 +42,7 @@ export default class App extends Component<Props> {
 
 
     Dialogflow.setConfiguration(
-      "1e0088a9ab7f4958aa04ab2ff8925070", Dialogflow.LANG_PORTUGUESE_BRAZIL
+      Constantes.ID_DIALOG_FLOW, Dialogflow.LANG_PORTUGUESE_BRAZIL
     );
   }
 
@@ -61,6 +70,40 @@ export default class App extends Component<Props> {
       error => console.log(error)
     );
 
+    this.ws.onmessage = evt => {
+      const retorno = JSON.parse(evt.data)
+      if(this.state.chatHumano) {
+        if(this.state.connectionId == null) {
+          this.state.connectionId = retorno.connectionId;
+        }
+
+        if(this.state.connectionId != null && this.state.connectionId == retorno.usuario) {
+         
+          const messages = {action : "onMessage" , message : retorno.data} 
+          if (this.state.ligarSom)
+                        Tts.speak(retorno.data);
+          this.onSend({
+            _id: Math.round(Math.random() * 1000000),
+            text: messages.message,
+            createdAt: new Date(),
+            user: {
+              _id: 3,
+              name: 'Chatbot Saraiva',
+              avatar: 'http://www.freelogovectors.net/photo6/Saraiva-logo.jpg',
+            }
+          });
+        }
+      }
+
+    }
+
+    this.ws.onclose = () => {
+      console.log('disconnected')
+      this.setState({
+        ws: new WebSocket(URL),
+      })
+    }
+
 
   }
 
@@ -68,6 +111,12 @@ export default class App extends Component<Props> {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }))
+  }
+
+  submitMessageWs = messageString => {
+    // on submitting the ChatInput form, send the message, add it to the list and reset the input
+    const message = {action : "onMessage" , message : messageString, usuario: "Atendente"} 
+    this.ws.send(JSON.stringify(message))
   }
 
   componentHideAndShow = () => {
@@ -83,28 +132,54 @@ export default class App extends Component<Props> {
           messages={this.state.messages}
           onSend={messages => {
             dismissKeyboard();
-            this.onSend(messages);
-            Dialogflow.requestQuery(
-              messages[0].text,
-              result => {
-                Tts.stop();
-                if (this.state.ligarSom)
-                  Tts.speak(result.result.fulfillment.messages[0].speech);
-                this.onSend({
-                  _id: Math.round(Math.random() * 1000000),
-                  text: result.result.fulfillment.messages[0].speech,
-                  createdAt: new Date(),
-                  user: {
-                    _id: 3,
-                    name: 'Chatbot Saraiva',
-                    avatar: 'http://www.freelogovectors.net/photo6/Saraiva-logo.jpg',
+
+            if(this.state.respostasIncorretas > 2) {
+              messages[0].text = 'chat humano';
+            }
+
+            if(messages[0].text.toUpperCase() == 'retornar bot'.toUpperCase()) {
+              messages[0].text = 'oi';
+              this.state.respostasIncorretas = 0;
+              this.state.chatHumano = false;
+              this.state.connectionId = null;
+            }
+
+            if(this.state.chatHumano == false) {
+              this.onSend(messages);
+              Dialogflow.requestQuery(
+                messages[0].text,
+                result => {
+                  Tts.stop();
+                  if (this.state.ligarSom)
+                    Tts.speak(result.result.fulfillment.messages[0].speech);
+  
+                  if(result.result.metadata.intentName == 'Default Fallback Intent') {
+                    this.state.respostasIncorretas++;
+                  } else {
+                    this.state.respostasIncorretas = 0;
                   }
-                });
-
-
-              },
-              error => console.log(error)
-            );
+  
+                  if(result.result.metadata.intentName == 'humano') {
+                    this.state.chatHumano = true;
+                  }
+  
+                  this.onSend({
+                    _id: Math.round(Math.random() * 1000000),
+                    text: result.result.fulfillment.messages[0].speech,
+                    createdAt: new Date(),
+                    user: {
+                      _id: 3,
+                      name: 'Chatbot Saraiva',
+                      avatar: 'http://www.freelogovectors.net/photo6/Saraiva-logo.jpg',
+                    }
+                  });
+                },
+                error => console.log(error)
+              );
+            } else {
+              this.submitMessageWs(messages[0].text)
+              this.onSend(messages)
+            }
           }}
         />
 
